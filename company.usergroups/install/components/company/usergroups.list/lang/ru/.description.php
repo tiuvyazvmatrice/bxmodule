f6b22a740cdab009995df991d996ecdaf04a159b
Игорь Амелин <i.amelin@qsoft.ru>
2021-09-22 15:36:50 +0300

    master правка описания компонента

--- company.usergroups/install/components/company/usergroups.list/lang/ru/.description.php
+++ company.usergroups/install/components/company/usergroups.list/lang/ru/.description.php
@@ -1,4 +1,3 @@
 <?php
-$MESS['COMPANY_U_DESC_LIST'] = "Список новостей";
-$MESS['COMPANY_U_DESC_LIST_DESC'] = "Список новостей из одного информационного блока.";
-$MESS['COMPANY_U_DESC_NEWS'] = "Статьи и новости";
+$MESS['COMPANY_U_DESC_LIST'] = "Список групп";
+$MESS['COMPANY_U_DESC_LIST_DESC'] = "Список групп с количеством участников";
--- company.usergroups/install/components/company/usergroups.list/lang/ru/templates/template.php
+++ company.usergroups/install/components/company/usergroups.list/lang/ru/templates/template.php
@@ -1,4 +1,4 @@
 <?php
-$MESS['COMPANY_U_GROUP_ID'] = "Список новостей";
-$MESS['COMPANY_U_GROUP_NAME'] = "Список новостей из одного информационного блока.";
-$MESS['COMPANY_U_COUNT'] = "Статьи и новости";
+$MESS['COMPANY_U_GROUP_ID'] = "ID группы";
+$MESS['COMPANY_U_GROUP_NAME'] = "Название группы";
+$MESS['COMPANY_U_COUNT'] = "Количество участников";
